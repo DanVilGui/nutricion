0818e5dd0c4f6b151b264451be6e2486df09e1fd
DanVilGui <vguivardanie@crece.uss.edu.pe>
2020-01-03 14:30:29 -0500

    agregar tips

---
+++ ws/tips.php
@@ -0,0 +1,26 @@
+<html>
+
+<body>
+ENLACES PARA LOS TIPS DE NUTRICIÓN
+<br>
+Si es posible que los enlaces vayan actualizándose con otros de la red.
+<br>
+La realidad de mitos y dietas:
+<br>
+https://comeronocomer.es/?fbclid=IwAR1YxIT6vvGthCaik0jTvxShNDs_DNBn4I7W5Npy3sgV32DXfuF-VakLFA
+Comidas saludables:
+https://www.tomadieta.com/?fbclid=IwAR0JR4OemZP1NmYMIBqhlNSkE6xujBE5R3P5w1314O
+XUn6dRV3rQeEcWHak
+Tips de alimentación:
+https://www.zonadiet.com/index.php?fbclid=IwAR3fht3HcSjnz83ilbwPGU4MdTzMtGzZy7z80
+w2diBPrI673CtjY0rHnvP4
+Nutrición y salud:
+https://nutricionysalud.org.es/
+Tips para una buena nutrición:
+https://www.clikisalud.net/8-tips-para-una-buena-nutricion/
+Consejos para mejorar la salud:
+https://es.familydoctor.org/nutricion-consejos-para-mejorar-su-salud/
+Los diez mandamientos de la alimentación:
+https://www.fucsia.co/belleza-y-salud/articulo/tips-de-nutricion-y-alimentacion-para-vidasana/72199
+</body>
+</html>
