9cdc2859cafd6716ac516382da84c79c471e69e1
DanVilGui <vguivardanie@crece.uss.edu.pe>
2020-01-08 14:17:30 -0500

    agregar 1

--- ws/registrar.php
+++ ws/registrar.php
@@ -39,6 +39,7 @@ if(isset($respuesta["success"])) {
     $login_tipo = $respuesta["login_tipo"];
     if($login_tipo =="NATIVO" or $rptidlogin_tipo != $clsPersona->getIdloginTipo()){
         $message = "";
+        echo $login_tipo;
         switch ($login_tipo){
             case "FACEBOOK": $message = "Este ya se encuentra registrado con FACEBOOK";break;
             case "GOOGLE": $message = "Este ya se encuentra registrado con GOOGLE";break;
